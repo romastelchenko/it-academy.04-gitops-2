2e433369eaf61a85e113e497ce17074feee2a1d5
Roman Stelchenko <romastelchenko@gmail.com>
2025-11-16 19:26:45 +0100

    [04.GitOps-2] add calc app, add workflow file

---
+++ calc/calc.py
@@ -0,0 +1,16 @@
+class CalcException(Exception):
+    pass
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise CalcException("Division by zero is not allowed")
+    return a / b
